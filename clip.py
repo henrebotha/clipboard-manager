52d4228d9b630c8e5fd064314433d28226fd9596
Henre Botha <henrebotha@gmail.com>
2014-03-30 13:30:35 +0200

    beginning GUI

--- clip.py
+++ clip.py
@@ -6,7 +6,7 @@ def run():
     root = Tk()
     root.title("Clippy")
     root.iconbitmap("icon2.ico")
-    root.geometry("250x195-0+0") # place window in NE (0 pix left, 0 pix down)
+    root.geometry("250x210-0+0") # place window in NE (0 pix left, 0 pix down)
     root.resizable(FALSE,FALSE)
 
     # function defs yo
@@ -14,30 +14,44 @@ def run():
         p = root.clipboard_get()
         print(p)
 
-    # widgets
-    clip_list = Text(root, height=10)
-    clip_list.pack()
-    button = Button(root, text="Paste", command=paste)
-    button.pack()
+    # configure widgets
+    # clip_list = Text(root, height=10, state=DISABLED)
+    labels_clips = [Label(root, text="BLAH", width=30) for i in range(10)]
+    labels_nums = [Label(root, text=str(i+1)[-1]+".") for i in range(10)]
+
+    for i, n, c in zip(range(10), labels_nums, labels_clips):
+        n.grid(column=0, row=i, sticky=E, padx=5)
+        c.grid(column=1, row=i)
+    
+    # place widgets
+    # clip_list.pack(fill=BOTH)
 
     # TODO: sort this out
     # # window sizing
-    # xy = str(root.winfo_reqwidth()) + "x" + str(root.winfo_reqheight())
+    # xy = str(root.winfo_width()) + "x" + str(root.winfo_reqheight())
     # root.geometry(xy + "-0+0")
 
     # event handlers
-    clip_list.bind("<Enter>", print("entering"))
-    clip_list.bind("<Leave>", print("leaving"))
+    # clip_list.bind("<Enter>", print("entering"))
+    # clip_list.bind("<Leave>", print("leaving"))
 
     # init clipboard system
     clip_buffer = root.clipboard_get()
     # clip_buffer = root.selection_get(selection="CLIPBOARD") # why use this??
-    clips = [clip_buffer]
-    runtime = 50
+    # clips = [clip_buffer, None, None, None, None, None, None, None, None, None]
+    # clips_string = ""
+    # for i, e in enumerate(clips):
+    #     clips_string += "{}.\t{}\n".format(str(i+1), str(e)[:16])
+    labels_clips[0].text = clip_buffer
+
+    # clip_list.config(state=NORMAL)
+    # clip_list.insert(END, clips_string)
+    # clip_list.config(state=DISABLED)
 
     root.mainloop()
 
     # TODO: put this inside mainloop using events or w/e
+    # runtime = 50
     # while True:
     #     clip = root.selection_get(selection="CLIPBOARD")
     #     if clip != clip_buffer:
